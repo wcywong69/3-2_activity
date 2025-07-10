92ea57233bf8aec4dce1822b2cdcd663c1facc19
wcywong69 <wcywong69@gmail.com>
2025-07-10 20:21:18 +0800

    Correct s3 backend key

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = ""  #Change this
+    key    = "terraform/state"  #Change this
     region = "ap-southeast-1"
   }
 }
